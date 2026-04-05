3cf86ba5e4339de95d92c1ecc44dc568aa2449b4
DonFredi <dondadi.fn@gmail.com>
2026-04-05 22:31:23 +0300

    pages added

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,3 +1,5 @@
+import MpesaForm from "@/components/ui/MpesaForm";
+
 export default function Form () {
     return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -5,7 +7,7 @@ export default function Form () {
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Mpesa Test Console
-            <MpesaForm />
+<MpesaForm />
           </h1>
         </div>
       </main>
