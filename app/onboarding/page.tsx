700dc9dc0279c5491deffc33fb3cabe6803691f0
DonFredi <dondadi.fn@gmail.com>
2026-04-06 20:14:00 +0300

    fixed oboarding

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -3,7 +3,7 @@ import OnboardingForm from "./OnboardingForm";
 
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div>Loading..</div>}>
       <OnboardingForm />
     </Suspense>
   );
