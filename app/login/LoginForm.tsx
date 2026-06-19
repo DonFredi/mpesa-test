71c753bc1e9c23ad15f23d62dc2ad16288b00156
DonFredi <dondadi.fn@gmail.com>
2026-06-19 15:56:42 +0300

    loginform fix

--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -74,7 +74,7 @@ export default function LoginPage() {
         />
 
         <button type="submit" disabled={loading} className="bg-black text-white px-4 py-2 w-full">
-          {loading ? "Signing In..." : "Login"}
+          {loading ? "Signing in..." : "Login"}
         </button>
 
         {error && <p className="text-red-500">{error}</p>}
