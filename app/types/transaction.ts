c80b471b0e1d671057b29d8080a9ead81af15cde
DonFredi <dondadi.fn@gmail.com>
2026-06-21 21:35:13 +0300

    types fix

--- app/types/transaction.ts
+++ app/types/transaction.ts
@@ -1,31 +1,31 @@
-export type Transaction {
+export type Transaction = {
   id: string;
-    clientId: string;
+  clientId: string;
   receipt?: string;
-  resultDesc:string;
+  resultDesc: string;
   phone?: string;
   amount?: number;
-  isTest:boolean;
+  isTest: boolean;
   fee?: number;
   netAmount?: number;
   transactionType?: string;
-   status: "pending" | "success" | "failed";
-     merchantRequestId?: string;
-     createdAt: any; // Firestore Timestamp
-     completedAt:any;
+  status: "pending" | "success" | "failed";
+  merchantRequestId?: string;
+  createdAt: any; // Firestore Timestamp
+  completedAt: any;
   updatedAt?: any;
-}
+};
 
 // export interface Transaction {
 //   id: string;
 
 //   phoneNumber: string;
 //   amount: number;
- 
+
 //   checkoutRequestId: string;
 
 //   mpesaReceiptNumber?: string;
 //   resultCode?: number;
 //   resultDesc?: string;
-  
+
 // }
